b3a3eaebeb18a0da119bafe45121a8c566cc5504
gutaosb <augustosemensato44@gmail.com>
2025-08-28 20:33:48 -0300

    ajustes finais no frontend - upload apenas de arquivos .txt e .pdf

--- app/frontend/src/components/FileUpload.jsx
+++ app/frontend/src/components/FileUpload.jsx
@@ -9,6 +9,20 @@ const FileUpload = ({ setLoading, setResult }) => {
 
     if (!file) return;
 
+    // valida extensão do arquivo
+    const validExtensions = [".txt", ".pdf"];
+    const fileExtension = file.name
+      .slice(file.name.lastIndexOf("."))
+      .toLowerCase();
+
+    if (!validExtensions.includes(fileExtension)) {
+      alert(
+        "Formato do arquivo não suportado. Apenas .txt e .pdf são aceitos."
+      );
+      e.target.value = ""; // limpa o input
+      return;
+    }
+
     setFilename(file.name);
     setLoading(true);
     setResult(null);
@@ -34,7 +48,12 @@ const FileUpload = ({ setLoading, setResult }) => {
     <div className="w-full max-w-lg mb-4">
       <label className="w-full cursor-pointer bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg inline-block text-center transition">
         📂 Escolher Arquivo
-        <input type="file" className="hidden" onChange={handleUpload} />
+        <input
+          type="file"
+          className="hidden"
+          onChange={handleUpload}
+          accept=".txt,.pdf"
+        />
       </label>
       {filename && (
         <p className="mt-2 text-gray-700">Arquivo selecionado: {filename}</p>
